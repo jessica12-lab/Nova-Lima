268d924c1893d9a7c6f85df47416f41b678ac8bc
Jessica Amaral <nike_jessica@hotmail.com>
2021-08-08 14:22:24 -0300

    finished

--- JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-function time(){
-  today = new Date();
-  d = today.getDate();
-  m = today.getMonth();
-  y = today.getFullYear();
-document.getElementById("date").innerHTML = d+":"+m+":"+y;
-setTimeout("date()")
-}
